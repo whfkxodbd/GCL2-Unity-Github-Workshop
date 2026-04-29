2bf4150f6332062bcaeaaec574d5a2b5247b10ca
rainbowseki22-source <rainbowseki22@gmail.com>
2026-04-29 12:48:10 +0800

    (Yumiko) changed player sprite, added health script, added health potion sprite to the scene

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Health restored!");
+    }
+}
